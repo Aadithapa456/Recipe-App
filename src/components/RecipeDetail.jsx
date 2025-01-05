53972c819374c784f78e323d45c212082adb7df5
Aaditya <aadityathapa977@gmail.com>
2025-01-05 23:13:42 +0545

    feat: improve recipe detail ui

--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -4,25 +4,36 @@ import { ArrowLeft, Clock, User } from "lucide-react";
 const capitalizeFirstLetter = (val) => {
   return String(val).charAt(0).toUpperCase() + String(val).slice(1);
 };
-
+const formatMinutes = (minutes) => {
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = minutes % 60;
+  if (hours > 0 && remainingMinutes > 0) {
+    return hours, remainingMinutes;
+  } else if (hours > 0) {
+    return `${hours}`;
+  } else {
+    return `${remainingMinutes}`;
+  }
+};
 const RecipeDetail = ({ data, close }) => {
   return (
     <>
       {data ? (
-        <div className="recipe-info-container mb-10 flex w-full flex-col gap-10">
+        <div className="recipe-info-container mx-auto mb-10 flex w-full max-w-[1400px] flex-col gap-10 px-10">
           <button
             className="transition duration-300 hover:text-primary"
             onClick={close}
           >
             <ArrowLeft />
           </button>
-          <div className="recipe-details mb-10 flex items-center justify-between">
+          <div className="recipe-meta-details mb-5 flex items-center justify-between">
             <div className="recipe-detail-title text-2xl font-bold text-primary">
               {data.title}
             </div>
             <div className="recipe-meta flex items-center gap-6 text-sm text-gray">
               <div className="recipe-time flex items-center gap-2">
-                <Clock /> <span>{data.cookingMinutes}</span>
+                {/* <Clock /> <span>{data.cookingMinutes || "15"} Mins</span> */}
+                <Clock /> <span>{formatMinutes(data.readyInMinutes)} Mins</span>
               </div>
               <div className="recipe-servings flex items-center gap-1">
                 <User />
@@ -30,49 +41,52 @@ const RecipeDetail = ({ data, close }) => {
               </div>
             </div>
           </div>
-          <div className="recipe-info-top flex gap-4">
+          <div className="recipe-info-top flex justify-between gap-10">
             <div className="image-container">
               <img
                 src={data.image}
                 alt={data.title}
-                className="h-[400px] w-full object-contain"
+                className="shadow-image h-[450px] w-full rounded-md object-cover"
               />
             </div>
-            <div className="recipe-info-ingredients h-fit w-4/5 rounded-lg px-6 py-4 shadow-md">
-              <div className="recipe-info-ingredients-title text-lg font-bold text-primary">
+            <div className="recipe-info-ingredients min-h-60 w-2/5 rounded-lg bg-white px-10 py-4 shadow-md">
+              <div className="recipe-info-ingredients-title py-3 text-xl font-bold text-primary">
                 Ingredients
               </div>
-              <div className="recipe-items grid-auto-fill">
-                {data.extendedIngredients.map((ingredient, index) => (
-                  <>
+              <Separator />
+              <div className="recipe-ingredient-items mb-4 mt-10 flex flex-col gap-3">
+                {data.extendedIngredients
+                  .slice(1, 7)
+                  .map((ingredient, index) => (
+                    // <>
                     <div
-                      className="recipe-info-item mt-4 flex h-fit cursor-pointer justify-between gap-2 rounded-lg bg-gray-light p-3 text-sm shadow-md transition duration-300 hover:-translate-y-1 hover:shadow-lg"
+                      className="recipe-ingredient-item shadow-xs flex items-center justify-between rounded-lg bg-gray-light px-3 py-2"
                       key={index}
                     >
-                      <div className="item-name">
+                      <div className="item-name text-base">
                         {capitalizeFirstLetter(ingredient.name)}
                       </div>
-                      <div className="item-quantity">
+                      <div className="item-quantity text-xs text-gray">
                         {ingredient.amount}
                         <span> {ingredient.unit}</span>
                       </div>
                     </div>
-                    {/* <Separator /> */}
-                  </>
-                ))}
+                    // </>
+                  ))}
               </div>
             </div>
           </div>
-          <div className="recipe-info-bottom flex gap-10">
-            <div className="recipe-info-instructions-wrapper flex h-fit flex-col rounded-lg px-4 pb-6 pt-3 shadow-md">
-              <div className="recipe-info-instructions-title px-4 text-xl font-bold text-primary">
+          <div className="recipe-info-bottom flex shadow-md">
+            <div className="recipe-info-instructions-wrapper flex h-fit flex-col rounded-lg bg-white px-8 py-6 shadow-md">
+              <div className="recipe-info-instructions-title py-2 text-2xl font-bold text-primary">
                 Instructions
               </div>
+              <Separator />
               <div className="recipe-instructions">
                 {data.analyzedInstructions[0].steps.map((item, index) => (
                   <>
-                    <div className="mt-8 flex items-center gap-4" key={index}>
-                      <div className="instruction-number flex h-10 w-10 items-center justify-center rounded-full bg-primary p-4 text-white">
+                    <div className="mt-8 flex items-center gap-6" key={index}>
+                      <div className="instruction-number flex h-6 w-6 items-center justify-center rounded-full bg-primary p-4 text-gray-light">
                         {index + 1}
                       </div>
                       <div className="instruction">{item.step}</div>
